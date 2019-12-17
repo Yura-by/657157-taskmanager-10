fe8f145b62d3fd42ccad7e6b364a2b6d7237d2ca
Yura-by <s.older681@gmail.com>
2019-12-17 18:26:30 +0300

    Данные теперь берутся из модели, фильтры перестали считать

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import BoardComponent from './components/board.js';
 import BoardController from './controllers/board.js';
 import SiteMenuComponent from './components/site-menu.js';
+import TasksModel from './models/tasks.js';
 import {generateTasks} from './mock/task.js';
 import {render, RenderPosition} from './utils/render.js';
 
@@ -12,10 +13,12 @@ const siteHeaderElement = siteMainElement.querySelector(`.main__control`);
 render(siteHeaderElement, new SiteMenuComponent(), RenderPosition.BEFOREEND);
 
 const tasks = generateTasks(TASK_COUNT);
+const tasksModel = new TasksModel();
+tasksModel.setTasks(tasks);
 
 const boardComponent = new BoardComponent();
 render(siteMainElement, boardComponent, RenderPosition.BEFOREEND);
 
-const boardController = new BoardController(boardComponent);
+const boardController = new BoardController(boardComponent, tasksModel);
 
-boardController.render(tasks);
+boardController.render();
