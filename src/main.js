7ff3272cb946dc2f5fab20b79f61e49247212425
Yura-by <s.older681@gmail.com>
2020-01-03 14:24:29 +0300

    Прошел 8.14

--- src/main.js
+++ src/main.js
@@ -1,22 +1,14 @@
+import API from './api.js';
 import BoardComponent from './components/board.js';
 import BoardController from './controllers/board.js';
 import FilterController from './controllers/filter.js';
 import SiteMenuComponent, {MenuItem} from './components/site-menu.js';
 import StatisticsComponent from './components/statistics.js';
 import TasksModel from './models/tasks.js';
-import {generateTasks} from './mock/task.js';
 import {render, RenderPosition} from './utils/render.js';
 
-const TASK_COUNT = 22;
-
-const siteMainElement = document.querySelector(`.main`);
-const siteHeaderElement = siteMainElement.querySelector(`.main__control`);
-const siteMenuComponent = new SiteMenuComponent();
-
-render(siteHeaderElement, siteMenuComponent, RenderPosition.BEFOREEND);
-const tasks = generateTasks(TASK_COUNT);
-const tasksModel = new TasksModel();
-tasksModel.setTasks(tasks);
+const AUTHORIZATION = `Basic dXNlckBwYJH:LKHkh`;
+const END_POINT = `https://htmlacademy-es-10.appspot.com/task-manager`;
 
 const dateTo = new Date();
 const dateFrom = (() => {
@@ -25,20 +17,25 @@ const dateFrom = (() => {
   return d;
 })();
 
+const api = new API(END_POINT, AUTHORIZATION);
+const tasksModel = new TasksModel();
+
+const siteMainElement = document.querySelector(`.main`);
+const siteHeaderElement = siteMainElement.querySelector(`.main__control`);
+const siteMenuComponent = new SiteMenuComponent();
 const statisticsComponent = new StatisticsComponent({tasks: tasksModel, dateFrom, dateTo});
 
-const filterController = new FilterController(siteMainElement, tasksModel);
-filterController.render();
-
 const boardComponent = new BoardComponent();
-render(siteMainElement, boardComponent, RenderPosition.BEFOREEND);
-
 const boardController = new BoardController(boardComponent, tasksModel);
+const filterController = new FilterController(siteMainElement, tasksModel);
+
+render(siteHeaderElement, siteMenuComponent, RenderPosition.BEFOREEND);
+filterController.render();
+render(siteMainElement, boardComponent, RenderPosition.BEFOREEND);
 
 render(siteMainElement, statisticsComponent, RenderPosition.BEFOREEND);
 
 statisticsComponent.hide();
-boardController.render();
 
 siteMenuComponent.setOnChange((menuItem) => {
   switch (menuItem) {
@@ -58,3 +55,9 @@ siteMenuComponent.setOnChange((menuItem) => {
       break;
   }
 });
+
+api.getTasks()
+  .then((tasks) => {
+    tasksModel.setTasks(tasks);
+    boardController.render();
+  });
