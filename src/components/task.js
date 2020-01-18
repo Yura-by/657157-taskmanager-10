75b123b1f10074397c4bb29a300c0cddb16105b4
Yura-by <s.older681@gmail.com>
2020-01-18 17:20:12 +0300

    Добавил debounce из lodash

--- src/components/task.js
+++ src/components/task.js
@@ -2,6 +2,9 @@ import AbstractComponent from './abstract-component.js';
 import {formatTime, formatDate} from '../utils/common.js';
 import {isOverdueDate} from '../utils/common.js';
 import he from 'he';
+import debounce from 'lodash/debounce';
+
+const DEBOUNCE_TIMEOUT = 500;
 
 const createHashtagsMarkup = (hashtags) => {
   return hashtags
@@ -108,11 +111,11 @@ export default class Task extends AbstractComponent {
 
   setFavoritesButtonClickHandler(handler) {
     this.getElement().querySelector(`.card__btn--favorites`)
-      .addEventListener(`click`, handler);
+      .addEventListener(`click`, debounce(handler, DEBOUNCE_TIMEOUT));
   }
 
   setArchiveButtonClickHandler(handler) {
     this.getElement().querySelector(`.card__btn--archive`)
-      .addEventListener(`click`, handler);
+      .addEventListener(`click`, debounce(handler, DEBOUNCE_TIMEOUT));
   }
 }
